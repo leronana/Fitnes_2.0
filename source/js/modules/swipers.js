c3f3312d0062fe711f3a853acd7de179fda1efd9
Валерия Тросянова <111880353+leronana@users.noreply.github.com>
2023-08-07 01:21:29 +0400

    закончила тренеры

--- source/js/modules/swipers.js
+++ source/js/modules/swipers.js
@@ -8,3 +8,31 @@ const swiperReviews = new Swiper('.reviews__swiper', {
     disabledClass: 'reviews__button-disabled',
   },
 });
+
+const swiperTrainers = new Swiper('.trainers__swiper', {
+  // Optional parameters
+  loop: true,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 5,
+    },
+    1360: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+
+  navigation: {
+    nextEl: '.trainers__swiper-button--next',
+    prevEl: '.trainers__swiper-button--prev',
+  },
+});
